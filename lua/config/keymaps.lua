b1e58100c26c2308f79943280bb4ff1bc831eb3d
caleb <caleb-webb@outlook.com>
2024-07-05 13:26:28 +1000

    keymap changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,9 +3,9 @@
 -- Add any additional keymaps here
 
 -- single-line
-vim.keymap.set("n", "<A-j>", ":move .+1<CR>==")
-vim.keymap.set("n", "<A-k>", ":move .-2<CR>==")
+vim.keymap.set("n", "<A-down>", ":move .+1<CR>==")
+vim.keymap.set("n", "<A-up>", ":move .-2<CR>==")
 
 -- multi-line
-vim.keymap.set("v", "<A-j>", ":move '>+1<CR>gv=gv")
-vim.keymap.set("v", "<A-k>", ":move '<-2<CR>gv=gv")
+vim.keymap.set("v", "<A-down>", ":move '>+1<CR>gv=gv")
+vim.keymap.set("v", "<A-up>", ":move '<-2<CR>gv=gv")
