4aafc032dcf555553f133426d354bc69d007a9ec
Caleb Webb <49512086+calebwebb@users.noreply.github.com>
2026-05-03 23:37:54 +0700

    Comments: keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,13 +3,13 @@ vim.g.maplocalleader = " "
 
 local map = vim.keymap.set
 
--- Lazy
-map("n","<leader>l", "<cmd>Lazy<CR>", {desc = "Lazy"})
-map("n","<leader>m", "<cmd>Mason<CR>", {desc = "Mason"})
+-- Leader commands
+map("n", "<leader>l", "<cmd>Lazy<CR>", { desc = "Lazy" })
+map("n", "<leader>m", "<cmd>Mason<CR>", { desc = "Mason" })
 
 -- Window navigation
 map("n", "<C-h>", "<C-w>h")
-map("n", "<C-j>","<C-w>j")
+map("n", "<C-j>", "<C-w>j")
 map("n", "<C-k>", "<C-w>k")
 map("n", "<C-l>", "<C-w>l")
 
